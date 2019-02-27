d510354d3f7999108db7a1a85dd469420a61a45d
Brandon Overton <igrproductions@gmail.com>
2019-02-26 20:46:39 -0600

    adding in are-we-up routing to verify it's up

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/are-we-up', (req, res) => res.sendStatus(200));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
