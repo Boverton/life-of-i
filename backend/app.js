da677df229c45d5dad2d685fa78e59639bc29610
brandonoverton <igrproductions@gmail.com>
2019-03-20 11:48:36 -0500

    Updates to backend routes  - using the registerRouter and pointing to /routes/register

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-/** routes **/
-const registerRoute = require('./routes/register');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
 
-app.use('/register', registerRoute);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/are-we-up', (req, res) => res.sendStatus(200));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+/** routes **/
+const registerRoute = require('./routes/register');
+
+app.use('/register', registerRoute);
