227da95c6daad740fd272aea24a345e34b1abec4
brandonoverton <igrproductions@gmail.com>
2019-03-20 17:42:01 -0500

    Implementation of axios interceptor.  Catching 400's and updating state   with the new messages from the API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,54 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Register from './components/auth/Register';
+import axios from 'axios';
+
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-          <Switch>
-            <Route path="/register" component={Register} />
-            <Route path="/" render={() => <h1>Home</h1>}/>
-          </Switch>
-        </Router>
-    );
+    constructor() {
+        super();
+
+        this.state = {
+            apiResponseMessages: {
+                type: "",
+                messages: [],
+            }
+        }
+    }
+
+    componentDidMount() {
+        axios.interceptors.response.use(response => {
+            let type = response.status === 400 ? "error" : "success";
+            this.updateMessages(response.data.errors, type);
+            return response;
+        })
+    }
+
+    updateMessages(messages, type) {
+        let apiResponseMessages = {...this.state.apiResponseMessages},
+            messagesArray = [];
+
+        if (messages.length !== 0) {
+            for (let index in messages) {
+                messagesArray.push(messages[index].message);
+            }
+        }
+
+        apiResponseMessages.type = type;
+        apiResponseMessages.messages = messagesArray;
+
+        this.setState({apiResponseMessages: apiResponseMessages});
+    }
+
+    render() {
+        return (
+            <Router>
+              <Switch>
+                <Route path="/register" component={Register} />
+                <Route path="/" render={() => <h1>Home</h1>}/>
+              </Switch>
+            </Router>
+        );
   }
 }
 
