95f32aee0de332bc77fd3a2314d5f3dde3c55e0e
brandonoverton <igrproductions@gmail.com>
2019-04-03 17:50:07 -0500

    Moving functions within correct scope now that this is being used for   multiple routes and not just register/

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ const dal = require('../lib/dal');
 const bcrypt = require('bcrypt');
 
 /**
- * register/ route
+ * auth/register route
  */
 router.post('/register', (req, res) => {
    (async () => {
@@ -31,36 +31,146 @@ router.post('/register', (req, res) => {
           return res.status(500).send(e.message);
       }
    })();
-});
 
-/**
- * Run through validation
- *   All fields have values
- *   username unique
- *   email is valid format
- *   email is unique
- *   password is long enough
- * @param body payload of request
- * @returns {Promise<Array>}
- */
-function validate(body) {
-   return new Promise(resolve => {
-      let errors = [];
-      (async () => {
-         await requireAll(body, errors);
+   /**
+    * Run through validation
+    *   All fields have values
+    *   username unique
+    *   email is valid format
+    *   email is unique
+    *   password is long enough
+    * @param body payload of request
+    * @returns {Promise<Array>}
+    */
+   function validate(body) {
+      return new Promise(resolve => {
+         let errors = [];
+         (async () => {
+            await requireAll(body, errors);
 
-         // if body fields aren't empty do validation on the values
-         if (errors.length === 0 ) {
-            await validateUsernameUnique(body.username, errors);
-            await validateEmailIsValid(body.email, errors);
-            await validateEmailUnique(body.email, errors);
-            await validatePasswordLongEnough(body.password, errors);
+            // if body fields aren't empty do validation on the values
+            if (errors.length === 0 ) {
+               await validateUsernameUnique(body.username, errors);
+               await validateEmailIsValid(body.email, errors);
+               await validateEmailUnique(body.email, errors);
+               await validatePasswordLongEnough(body.password, errors);
+            }
+
+            return resolve(errors);
+         })();
+      });
+   }
+
+
+   /**
+    * Validates the username is unique and doesn't exist in users.username
+    *
+    * @param username
+    * @param errors
+    * @returns {Promise<Array>}
+    */
+   function validateUsernameUnique(username, errors) {
+      return new Promise( resolve => {
+         (async () => {
+            let isUnique = await dal.isUnique("users", {username: username});
+            if (!isUnique) {
+               errors.push({
+                  field: "username",
+                  message: "Username already taken, please choose another."
+               });
+            }
+            return resolve(errors);
+         })();
+      });
+   }
+
+   /**
+    * Validates email is proper format
+    *
+    * @param email
+    * @param errors
+    * @returns {Promise<Array>}
+    */
+   function validateEmailIsValid(email, errors) {
+      return new Promise(resolve => {
+         if (!validateEmailAddress(email)) {
+            errors.push({
+               field: "email",
+               message: email + " is not a valid email address."
+            });
          }
 
          return resolve(errors);
-      })();
-   });
-}
+      });
+   }
+
+   /**
+    * Validates email is unique  and doesn't exist in users.emai
+    *
+    * @param email
+    * @param errors
+    * @returns {Promise<Array>}
+    */
+   function validateEmailUnique(email, errors) {
+      return new Promise(resolve => {
+         (async () => {
+            let isUnique = await dal.isUnique("users", {email: email});
+            if (!isUnique) {
+               errors.push({
+                  field: "email",
+                  message: "Email is already registered, please log in."
+               });
+            }
+            return resolve(errors);
+         })();
+      });
+   }
+
+   /**
+    * Validates password is long enough (> 5 char)
+    *
+    * @param password
+    * @param errors
+    * @returns {Promise<Array>}
+    */
+   function validatePasswordLongEnough(password, errors) {
+      return new Promise( resolve => {
+         if (password.length < 5) {
+            errors.push({
+               field: "password",
+               message: "Password must be more than 5 characters."
+            });
+         }
+         return resolve(errors)
+      });
+   }
+
+   /**
+    * Using bcrypt generate and return hashed password
+    *
+    * @param password
+    * @returns {Promise<String>}
+    */
+   function hashPassword(password) {
+      return new Promise( resolve => {
+         bcrypt.hash(password, Number(process.env.B_SALT_ROUND), function (err, hash) {
+            return resolve(hash);
+         });
+      });
+   }
+
+   /**
+    * covers 95% email validation
+    * https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    * @param email
+    * @returns {boolean}
+    */
+   function validateEmailAddress(email) {
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(email).toLowerCase());
+   }
+});
+
 
 /**
  * Validates all fields in request have a value
@@ -84,88 +194,9 @@ function requireAll(body, errors) {
    })
 }
 
-/**
- * Validates the username is unique and doesn't exist in users.username
- *
- * @param username
- * @param errors
- * @returns {Promise<Array>}
- */
-function validateUsernameUnique(username, errors) {
-   return new Promise( resolve => {
-      (async () => {
-         let isUnique = await dal.isUnique("users", {username: username});
-         if (!isUnique) {
-            errors.push({
-               field: "username",
-               message: "Username already taken, please choose another."
-            });
-         }
-         return resolve(errors);
-      })();
-   });
-}
-
-/**
- * Validates email is proper format
- *
- * @param email
- * @param errors
- * @returns {Promise<Array>}
- */
-function validateEmailIsValid(email, errors) {
-   return new Promise(resolve => {
-      if (!validateEmailAddress(email)) {
-         errors.push({
-            field: "email",
-            message: email + " is not a valid email address."
-         });
-      }
-
-      return resolve(errors);
-   });
-}
-
-/**
- * Validates email is unique  and doesn't exist in users.emai
- *
- * @param email
- * @param errors
- * @returns {Promise<Array>}
- */
-function validateEmailUnique(email, errors) {
-   return new Promise(resolve => {
-      (async () => {
-         let isUnique = await dal.isUnique("users", {email: email});
-         if (!isUnique) {
-            errors.push({
-               field: "email",
-               message: "Email is already registered, please log in."
-            });
-         }
-         return resolve(errors);
-      })();
-   });
-}
-
-/**
- * Validates password is long enough (> 5 char)
- *
- * @param password
- * @param errors
- * @returns {Promise<Array>}
- */
-function validatePasswordLongEnough(password, errors) {
-   return new Promise( resolve => {
-      if (password.length < 5) {
-         errors.push({
-            field: "password",
-            message: "Password must be more than 5 characters."
-         });
-      }
-      return resolve(errors)
-   });
-}
+/*****
+ * General funcs
+ *****/
 
 /**
  * Capitalizes first letter of a string
@@ -177,29 +208,5 @@ function capitalizeFirstLetter(string) {
    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-/**
- * Using bcrypt generate and return hashed password
- *
- * @param password
- * @returns {Promise<String>}
- */
-function hashPassword(password) {
-   return new Promise( resolve => {
-      bcrypt.hash(password, Number(process.env.B_SALT_ROUND), function (err, hash) {
-         return resolve(hash);
-      });
-   });
-}
-
-/**
- * covers 95% email validation
- * https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
- * @param email
- * @returns {boolean}
- */
-function validateEmailAddress(email) {
-   let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return re.test(String(email).toLowerCase());
-}
 
 module.exports = router;
